d94730bbae8dec0cd59c293bb4db675f9315b221
Addisu <Addisu@twinbosses.com>
2020-12-05 18:10:23 +0300

    Added Authentication Controller

---
+++ WebAPI/Dtos/dtos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Dtos
+{
+    public class dtos
+    {
+    }
+
+    public class GetByCode
+    {
+        public string id { get; set; }  //Organization TIN
+        public string itemCode { get; set; }  //Object Code
+    }
+
+    public class GetByDateRange
+    {
+        public string id { get; set; }  //Organization TIN
+        public string fromDate { get; set; }  //Start Date
+        public string toDate { get; set; }  //End Date
+        public string source { get; set; }  //End Date
+        public string destination { get; set; }  //End Date
+    }
+}
